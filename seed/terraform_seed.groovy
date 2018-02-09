2ba45a5ce956e487cb973021242550fed161592e
Cole <mc185308@wcamc185308-kt2.corp.ncr.com>
2018-02-09 15:20:31 -0500

    fixed script name

---
+++ seed/terraform_seed.groovy
@@ -0,0 +1,19 @@
+folder('devops')
+folder('devops/Build')
+folder('devops/Build/terraform')
+folder('devops/Build/terraform')
+pipelineJob('devops/Build/terraform/build_terraform_environment') {
+  def repo = 'https://github.com/mc1985/pipeline.git'
+  definition {
+    cpsScm {
+      scm {
+        git {
+           remote { url(repo) }
+           scriptPath('pipeline/terraformrun.groovy')
+           extensions {}
+
+            }
+          }
+        }
+      }
+    }
