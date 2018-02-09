205ee50a0ef15fd265ef61353223b36443e637a8
Cole <mc185308@wcamc185308-kt2.corp.ncr.com>
2018-02-09 15:08:58 -0500

    fix

--- pipeline/terraformrun.groovy
+++ pipeline/terraformrun.groovy
@@ -1,16 +1,7 @@
 // Pull code from git
 node {
-      stage "Pull code from git"
-      scm {
-          git {
-              remote {
-                  name('terraform module')
-                  url('https://github.com/mc1985/pipeline.git')
-        }
-          }
-            }
-
-}
+    stage "Pull code from git"
+    git url: "https://github.com/mc1985/pipeline.git", branch: 'master'
 // Build new terraform images
     stage "Build Docker Image"
     sh "docker build -f docker/terraform_base/Dockerfile -t terraform_base ."
@@ -18,3 +9,4 @@ node {
    stage "Run Terraform Image"
     sh "docker run terraform_base"
 // push state file back to git repo
+}
