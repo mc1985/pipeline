bff31b5c0959b5f7628ab465062b32614c00b7af
Cole <mc185308@wcamc185308-kt2.corp.ncr.com>
2018-02-15 11:58:20 -0500

    updated to use cusotm docker image

--- pipeline/stage_appstack_terraform_create.groovy
+++ pipeline/stage_appstack_terraform_create.groovy
@@ -1,12 +1,13 @@
 // Pull code from git
 node {
-    stage "Pull code from git"
+    stage ("Pull code from git")
     git url: "https://github.com/mc1985/pipeline.git", branch: 'master'
 // Build new terraform images
-    stage "Build Docker Image"
+    stage ("Build Docker Image")
     sh "docker build -f docker/terraform_base/Dockerfile -t terraform_base ."
 // Run terraform container
-   stage "Build custom Terraform Image"
-    sh "docker run terraform_base init"
+    stage ("run custom Terraform Image"){
+    withCredentials([file(credentialsId:'terrafrom.tfvars', variable:'TFVARS')]) {
+    sh "docker run -t --rm -e ACTION=plan -v $TFVARS=env-vars/terraform.tfvars terraform_custom"
 // push state file back to git repo
 }
