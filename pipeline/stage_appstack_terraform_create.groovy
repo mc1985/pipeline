727d80bfaa00dc800ef8f3555ee9b661b6a06890
Cole <mc185308@wcamc185308-kt2.corp.ncr.com>
2018-02-16 08:16:35 -0500

    changed docker run

--- pipeline/stage_appstack_terraform_create.groovy
+++ pipeline/stage_appstack_terraform_create.groovy
@@ -3,12 +3,13 @@ node {
     stage ("Pull code from git")
     git url: "https://github.com/mc1985/pipeline.git", branch: 'master'
 // Build new terraform images
-    stage ("Build Docker Image")
-    sh "docker build -f docker/terraform_base/Dockerfile -t terraform_base ."
+    stage ("Build Docker Image") {
+    sh "docker build -f Dockerfile -t terraform_base ."
+  }
 // Run terraform container
     stage ("run custom Terraform Image"){
     withCredentials([file(credentialsId:'terrafrom.tfvars', variable:'TFVARS')]) {
-    sh "docker run -t --rm -e ACTION=plan -v $TFVARS=env-vars/terraform.tfvars terraform_custom"
+    sh "docker run -t --rm -e ACTION=plan -v $TFVARS=env-vars/terraform.tfvars terraform_base"
   }
 }
 // push state file back to git repo
